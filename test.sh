54ed810e797caf7370f546380c49618a384a13a0
root <root@localhost.localdomain>
2020-11-10 01:24:30 +0900

    second

---
+++ test.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+yum install https://repo.ius.io/ius-release-el7.rpm https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
+yum install  python36u python36u-libs python36u-devel python36u-pip python36u-mod_wsgi -y
+yum install git -y
+
+python3.6 -m pip install --upgrade pip
+pip3.6 install django~=2.1.5
+pip3.6 install virtualenvwrapper
+pip3.6 install mysqlclient
+
+firewall-cmd --add-port=80/tcp --zone=public --permanent
+firewall-cmd --reload
+echo "export WORKON_HOME=~/ENV" >> ~/.bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=/bin/python3.6" >> ~/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+
+source ~/.bashrc
+mkvirtualenv test
+workon test
+
+mkdir -p /usr/local/django
+cd /usr/local/django
+
+git init
+git clone https://github.com/woo3848/djnago_pro.git /usr/local/django
+
+
+echo "[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/7/$basearch/
+gpgcheck=0
+enabled=1" > /etc/yum.repos.d/nginx.repo
+
+yum install nginx -y
+systemctl start nginx
+systemctl enable nginx
+
+yum install -y gcc
+pip3.6 install uwsgi
+
+mkidr -p /etc/uwsgi/sites
+echo "[uwsgi]
+project = myproject
+username = root
+base = /root
+
+### Django Settings
+# base directory
+chdir = /usr/local/django/project
+# python path
+home = /root/Env/test/bin/python
+# virtualenv path
+virtualenv = /root/Env/test
+# wsgi.py path
+module = project.wsgi:application
+
+master = true
+processes = 5
+
+uid = root
+socket = /run/uwsgi/project.sock
+chown-socket = root:nginx
+chmod-socket = 660
+vacuum = true
+
+logto = /var/log/uwsgi/project.log" > /etc/uwsgi/sites/project.ini
+
+mkdir -p /var/log/uwsgi
+
+echo "[Unit]
+Description=uWSGI service
+
+[Service]
+
+ExecStartPre=/bin/mkdir -p /run/uwsgi
+
+ExecStartPre=/bin/chown root:nginx /run/uwsgi
+
+ExecStart=/root/Env/test/bin/uwsgi --emperor /etc/uwsgi/sites
+Restart=always
+Type=notify
+NotifyAccess=all
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/uwsgi.service
+
+systemctl daemon-reload
+
+ips = `/sbin/ifconfig | grep '\<inet\>' | sed -n '1p' | tr -s ' ' | cut -d ' ' -f3 | cut -d ':' -f2`
+echo "server{
+
+        listen 8000;
+
+        server_name $ips;
+
+
+
+        location / {
+
+                include uwsgi_params;
+
+                uwsgi_pass unix:/run/uwsgi/project.sock;
+
+        }
+
+}" > /etc/nginx/conf.d/project.sock
+
+systemctl start uwsgi
+systemctl enable uwsgi
+
+# sed -i "s/ALLOWED_HOSTS.*/ALLOWED_HOSTS = [$ips]/" second.txt
+
